c33edf57c20590d7f504e40e45934d8298eafad4
jer11698 <32538619+jer11698@users.noreply.github.com>
2022-07-12 10:49:16 +0700

    Update db.js

--- src/data/db.js
+++ src/data/db.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/database";
 
-var firebaseConfig = {
+/*var firebaseConfig = {
   apiKey: "AIzaSyCOOs4sh8y1UDaOyaWdmk4e3O0EttFRVfk",
   authDomain: "web-design-project-28643.firebaseapp.com",
   projectId: "web-design-project-28643",
@@ -9,6 +9,17 @@ var firebaseConfig = {
   messagingSenderId: "903569791597",
   appId: "1:903569791597:web:6f3d97880bbab83e9f7d64",
   measurementId: "G-D4KLLG9FLQ"
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2E6bwOHZI8yCqZ1lR7Fq_dB20xDW-NC0",
+  authDomain: "hhgg-data.firebaseapp.com",
+  databaseURL: "https://hhgg-data-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hhgg-data",
+  storageBucket: "hhgg-data.appspot.com",
+  messagingSenderId: "448917902495",
+  appId: "1:448917902495:web:3d31b33f4c35857937beaa",
+  measurementId: "G-0FYPSQBZPN"
 };
 
 export const db = firebase
